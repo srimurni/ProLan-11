2270c491ffec87537e09369fe1f030edda184327
srimurni <srimurnilestari94@gmail.com>
2017-02-28 21:39:14 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,10 @@
+public class Mahasiswa
+{
+public static void main(String []args)
+	{
+System.out.println("Nama   : Sri Murni Lestari");
+System.out.println("Nim    : 10113061");
+System.out.println("Kelas  : Prolan-11");
+System.out.println("Alamat : Tubagus Ismail Dalam");
+	}
+}
